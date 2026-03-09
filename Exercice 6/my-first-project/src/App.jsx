5e80033c9c80e436b4646733e349b165d25a9eb9
Najatelkadiri <156453019+Najatelkadiri@users.noreply.github.com>
2026-03-09 09:52:49 +0000

    exercice-6

--- Exercice 6/my-first-project/src/App.jsx
+++ Exercice 6/my-first-project/src/App.jsx
@@ -1,31 +1,51 @@
-import MessageBienvenue from "./MessageBienvenue.jsx"
-import BookCard from "./BookCard.jsx"
+import { useEffect, useState } from 'react'
+
+function BookLike({ titre, auteur }) {
+  return (
+    <div style={{
+      background: "#1e293b",
+      color: "white",
+      padding: "20px",
+      marginTop: "20px",
+      borderRadius: "15px"
+    }}>
+      <h3>{titre}</h3>
+      <p>{auteur}</p>
+    </div>
+  )
+}
 
 function App() {
-    return (
-        <div>
-            <h1>Ma bibliothèque en React</h1>
-          
-            <MessageBienvenue />
-            <BookCard 
-                titre="Le Petit Prince" 
-                auteur="Antoine de Saint-Exupéry" 
-                categorie="Roman" 
-            />
-            
-            <BookCard 
-                titre="Les Misérables" 
-                auteur="Victor Hugo" 
-                categorie="Littérature" 
-            />
-            
-            <BookCard 
-                titre="Clean Code" 
-                auteur="Robert C. Martin" 
-                categorie="Informatique" 
-            />
-        </div>
-    )
+  const [livre, setLivre] = useState("Le Petit Prince")
+
+  useEffect(() => {
+    document.title = `Livre : ${livre}`
+  }, [livre])
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Ma bibliothèque en React</h1>
+
+      <p>Livre sélectionné : <strong>{livre}</strong></p>
+
+      <button onClick={() => setLivre("Le Petit Prince")}>
+        Livre 1
+      </button>
+
+      <button onClick={() => setLivre("Les Misérables")}>
+        Livre 2
+      </button>
+
+      <button onClick={() => setLivre("Clean Code")}>
+        Livre 3
+      </button>
+
+      <BookLike 
+        titre={livre}
+        auteur="Auteur variable"
+      />
+    </div>
+  )
 }
 
 export default App
